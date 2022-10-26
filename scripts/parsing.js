bae0ff1acbe3c2d1000280c518c97e1d4ef80a33
Khanim Humbatova <khumbatova@pgh.yearup.org>
2022-10-26 15:55:38 -0400

    add h1 , p tag to index.html and add functions to parsing.js

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,32 @@
-console.log('the script tag works');
+function parseAndDisplayName(name) {
+    let space = name.indexOf(' ');
+    let firstName = name.substring(0, space);
+    let lastName = name.substring(space + 1);
+
+    console.log(firstName, lastName, name);
+
+    let spanLast = document.getElementById('last');
+    spanLast.innerText = lastName;
+
+    let spanFirst = document.getElementById('first');
+    spanFirst.innerText = firstName;
+    
+    let spanFull = document.getElementById('full');
+    spanFull.innerText = name;
+
+}
+
+parseAndDisplayName('Robert Deniro');
+
+
+
+
+// console.log('the script tag works');
+// let fullName = "Kevin Ruse";
+// console.log(`the variable fullName is: ${fullName}`);
+// let space = fullName.indexOf(' ');
+// console.log(space);
+// let firstName = fullName.substring(0, space);
+// console.log(firstName);
+// let lastName = fullName.substring(space + 1);
+// console.log(lastName);
